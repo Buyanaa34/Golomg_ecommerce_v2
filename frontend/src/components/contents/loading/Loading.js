fe546545fe480dfd84c8f2a501bdf2f3e0d8bdf8
Buyankhishig <68805269+Buyanaa34@users.noreply.github.com>
2021-07-24 11:50:52 +0800

    Initial commit

---
+++ frontend/src/components/contents/loading/Loading.js
@@ -0,0 +1,22 @@
+import { Height } from "@material-ui/icons";
+import React from "react";
+import Lottie from "react-lottie";
+import * as loading from "../../../67771-star-sine.json";
+import "./style.css";
+function Loading() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loading.default,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="loader_container">
+      <Lottie options={defaultOptions} height={"45%"} width={"45%"}></Lottie>
+    </div>
+  );
+}
+
+export default Loading;
