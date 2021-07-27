197ff225a472c85481bb4796a8b002c7e6bfa9e0
Buyankhishig <68805269+Buyanaa34@users.noreply.github.com>
2021-07-27 15:15:35 +0800

    Update Home.js

--- frontend/src/components/contents/pages/Home.js
+++ frontend/src/components/contents/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
   };
   return (
     <div className="pagez">
-      <h1 className="welcoming">Голомт банк интернэт худалдаа тест хуудас</h1>
+      <h1 className="welcoming">Голомт банк интернет худалдаа тест хуудас</h1>
       <div className="home_holder">
         {/* <Lottie options={defaultOptions} speed="1.6"></Lottie> */}
         <img className="nvvr_pic" src="/bank.jpg" alt="" />
